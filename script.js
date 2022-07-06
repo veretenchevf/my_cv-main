0d777f97cbe213a20a131f8c15a742c26036d5a0
Veretenchev Fedor <veretenchevf@gmail.com>
2022-07-06 15:48:18 +0300

    feat: add portfolio links

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const MENU_BOX = document.querySelector("#home > nav");
 const BURGER = document.querySelector("#home > label");
+const IMGS = document.querySelectorAll("[data-url]")
 
+console.log(IMGS)
 BURGER.addEventListener('click', () => {
     MENU_BOX.classList.toggle('active__menu')
 })
 
 MENU_BOX.addEventListener('click', () => {
     BURGER.click()
+})
+
+IMGS.forEach(img => {
+    img.addEventListener("click", (click) => {
+        let url = click.target.getAttribute('data-url');
+        window.open(`${url}`, '_blank');
+    })
 })
